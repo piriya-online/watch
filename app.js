76d635f8304faa33c4a58f39439cb1d751f69450
ThaiSmartDev <dev@thaismartdev.com>
2019-02-12 21:10:31 +0700

    Update

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var watch = require('node-watch')
+    , fs = require('fs')
     , config = require('./config.js')
  
-watch('./xx', { recursive: true }, function(evt, name) {
-  console.log('%s changed.', name);
+watch(config.path, { recursive: true }, function(evt, name) {
+    if( name.toLowerCase().indexOf('.jpg') != -1 ||
+        name.toLowerCase().indexOf('.jpeg') != -1 ||
+        name.toLowerCase().indexOf('.png') != -1 ||
+        name.toLowerCase().indexOf('.gif') != -1
+    ){
+        fs.appendFile('test.txt', name+"\n", function (err) {
+        });
+    }
 });
